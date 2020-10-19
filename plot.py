de24c512ae184a5ec760c65c3bd148744d72b7e8
effepivi <franck.p.vidal@gmail.com>
2020-10-19 14:44:12 +0100

    Add compiler in legend.

--- plot.py
+++ plot.py
@@ -11,8 +11,9 @@ matplotlib.use('Qt5Agg')
 
 df = pd.read_csv("runtimes.csv");
 
-fig = plt.figure(figsize=(8, 6));
+fig = plt.figure(figsize=(11, 6));
 ax = plt.subplot(111);
+
 
 for processor in df["Processor"].unique():
     test1=df["Processor"] == processor;
@@ -20,20 +21,24 @@ for processor in df["Processor"].unique():
     for compilation in df["Compilation"].unique():
         test2=df["Compilation"] == compilation;
         
-        X = [];
-        Y = [];
-        
-        x = df[test1 & test2]["Number of threads"];
-        y = df[test1 & test2]["Runtime in seconds"];
-        
-        for temp_x, temp_y in zip(x, y):
-            if not math.isnan(temp_y):
-                X.append(temp_x);
-                Y.append(temp_y);
+        for compiler in df["Compiler"].unique():
+            test3=df["Compiler"] == compiler;
 
-        if len(X) and len(Y):
-            plt.plot(X, Y, label=processor + " (compilation: " + compilation + ")");
-        
+            X = [];
+            Y = [];
+            
+            x = df[test1 & test2 & test3]["Number of threads"];
+            y = df[test1 & test2 & test3]["Runtime in seconds"];
+            
+            for temp_x, temp_y in zip(x, y):
+                if not math.isnan(temp_y):
+                    X.append(temp_x);
+                    Y.append(temp_y);
+
+            if len(X) and len(Y):
+                plt.plot(X, Y, label=processor + " (compiler: " + compiler + ", compilation: " + compilation + ")");
+
+
 ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
           ncol=1, fancybox=True, shadow=True);
 
