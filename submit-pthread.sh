723465334e094e10d1ec8002d3321adbebbaed8b
Franck Vidal <franck.p.vidal@gmail.com>
2020-11-12 17:45:53 +0000

    Scripts submitting severl jobs instead of a big one.

--- submit-pthread.sh
+++ submit-pthread.sh
@@ -1,47 +1,63 @@
 #!/usr/bin/env bash
-#
-#SBATCH -A scw1563                   # Project/Account (use your own)
-##SBATCH --mail-user=YOUREMAILADDRESS@bangor.ac.uk  # Where to send mail
-#SBATCH --mail-type=END,FAIL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --job-name=RT-pthreads       # Job name
-#SBATCH --output ray_tracing-%j.out  #
-#SBATCH --error ray_tracing-%j.err   #
-#SBATCH --nodes=1                    # Use one node
-#SBATCH --ntasks-per-node=1          # Number of tasks per node
-#SBATCH --cpus-per-task=1            # Number of cores per task
-#SBATCH --mem=600mb                  # Total memory limit
-#SBATCH --time=10:00:00              # Time limit hrs:min:sec
 
-# Clear the environment from any previously loaded modules
-module purge > /dev/null 2>&1
-module load cmake
-
-COMPILER=`gcc --version |head -1`
-
-# Uncomment if your are using the intel compiler
-#module load compiler/intel/2020/2 #compiler/gnu/9/2.0
-#COMPILER=`icc --version |head -1`
-
-TEMP=`lscpu|grep "Model name:"`
-IFS=':' read -ra CPU_MODEL <<< "$TEMP"
-
-width=128
-height=128
-
-if [ ! -f timing.csv ];
-then
-    echo "CPU,Parallelisation,Number of threads per node,Number of nodes,Compiler,Image size,Runtime in sec" > timing.csv
-fi
+width=2048
+height=2048
 
 for thread_number in 160 80 40 24 16 8 4 1
 do
-    echo Run ./main-pthreads with $thread_number threads.
-    
-    /usr/bin/time --format='%e' ./main-pthreads --size $width $height --jpeg pthreads-$thread_number-${width}x$height.jpg --threads $thread_number 2> temp
+	echo "#!/usr/bin/env bash" > submit-pthread-$thread_number.sh
+	echo "#" >> submit-pthread-$thread_number.sh
+	echo "#SBATCH -A scw1563                   # Project/Account (use your own)" >> submit-pthread-$thread_number.sh
+	echo "##SBATCH --mail-user=YOUREMAILADDRESS@bangor.ac.uk  # Where to send mail" >> submit-pthread-$thread_number.sh
+	echo "#SBATCH --mail-type=END,FAIL         # Mail events (NONE, BEGIN, END, FAIL, ALL)" >> submit-pthread-$thread_number.sh
+	echo "#SBATCH --job-name=RT-$thread_number-pthreads       # Job name" >> submit-pthread-$thread_number.sh
+	echo "#SBATCH --output ray_tracing-%j.out  #" >> submit-pthread-$thread_number.sh
+	echo "#SBATCH --error ray_tracing-%j.err   #" >> submit-pthread-$thread_number.sh
+	echo "#SBATCH --nodes=1                    # Use one node" >> submit-pthread-$thread_number.sh
+	echo "#SBATCH --ntasks-per-node=1          # Number of tasks per node" >> submit-pthread-$thread_number.sh
 
-    RUNTIME=`cat temp`
+ 	if [ "$thread_number" -ge "40" ]
+	then
+		echo "#SBATCH --cpus-per-task=40            # Number of cores per task" >> submit-pthread-$thread_number.sh
+		echo "#SBATCH --time=00:25:00              # Time limit hrs:min:sec" >> submit-pthread-$thread_number.sh
+	else
+		echo "#SBATCH --cpus-per-task=$thread_number            # Number of cores per task" >> submit-pthread-$thread_number.sh
+		echo "#SBATCH --time=00:50:00              # Time limit hrs:min:sec" >> submit-pthread-$thread_number.sh
+	fi
 
-    echo ${CPU_MODEL[1]},Pthread,$thread_number,1,$COMPILER,${width}x$height,$RUNTIME >> timing.csv
-    rm temp
+	echo "#SBATCH --mem=600mb                  # Total memory limit" >> submit-pthread-$thread_number.sh
 
+	echo "thread_number=$thread_number"  >> submit-pthread-$thread_number.sh
+
+	# Clear the environment from any previously loaded modules
+	echo "module purge > /dev/null 2>&1" >> submit-pthread-$thread_number.sh
+	echo "module load cmake" >> submit-pthread-$thread_number.sh
+
+	echo "COMPILER=\"`gcc --version |head -1`\"" >> submit-pthread-$thread_number.sh
+
+	# Uncomment if your are using the intel compiler
+	#module load compiler/intel/2020/2 #compiler/gnu/9/2.0
+	#COMPILER=`icc --version |head -1`
+
+	echo "TEMP=\`lscpu|grep \"Model name:\"\`" >> submit-pthread-$thread_number.sh
+	echo "IFS=':' read -ra CPU_MODEL <<< \"\$TEMP\"" >> submit-pthread-$thread_number.sh
+
+	echo "width=$width" >> submit-pthread-$thread_number.sh
+	echo "height=$height" >> submit-pthread-$thread_number.sh
+
+	if [ ! -f timing.csv ];
+	then
+		echo "CPU,Parallelisation,Number of threads per node,Number of nodes,Compiler,Image size,Runtime in sec" > timing.csv
+	fi
+
+	echo "echo Run ./main-pthreads with $thread_number threads." >> submit-pthread-$thread_number.sh
+
+	echo "/usr/bin/time --format='%e' ./bin-release-gcc/main-pthreads --size $width $height --jpeg pthreads-$thread_number-${width}x$height.jpg --threads $thread_number 2> temp-$thread_number" >> submit-pthread-$thread_number.sh
+
+	echo "RUNTIME=\`cat temp-$thread_number\`" >> submit-pthread-$thread_number.sh
+
+  echo "echo \${CPU_MODEL[1]},Pthread,\$thread_number,1,\$COMPILER,\${width}x\$height,\$RUNTIME >> timing-pthread-$thread_number.csv" >> submit-pthread-$thread_number.sh
+  echo "rm temp-$thread_number" >> submit-pthread-$thread_number.sh
+	chmod +x submit-pthread-$thread_number.sh
+	sbatch submit-pthread-$thread_number.sh
 done
