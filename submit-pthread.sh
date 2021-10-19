e3bc30d2367f8b0a23e0faf47ebca07a86183cf3
effepivi <franck.p.vidal@fpvidal.net>
2021-10-19 14:27:36 +0100

    Update submit-pthread.sh
    Use the GNU compiler by default.

--- submit-pthread.sh
+++ submit-pthread.sh
@@ -3,10 +3,7 @@
 width=2048
 height=2048
 
-module purge > /dev/null
-module load cmake mpi/intel
-
-for thread_number in 16 #160 80 40 24 16 8 4 1
+for thread_number in 1 2 4 8 16 24 32 40 80 160
 do
 	echo "#!/usr/bin/env bash" > submit-pthread-$thread_number.sh
 	echo "#" >> submit-pthread-$thread_number.sh
@@ -34,14 +31,13 @@ do
 	echo "thread_number=$thread_number"  >> submit-pthread-$thread_number.sh
 
 	# Clear the environment from any previously loaded modules
-	echo "module purge > /dev/null 2>&1" >> submit-pthread-$thread_number.sh
-	echo "module load cmake mpi/intel" >> submit-pthread-$thread_number.sh
+	echo "source env-gnu.sh" >> submit-pthread-$thread_number.sh
 
 	echo "COMPILER=\"`gcc --version |head -1`\"" >> submit-pthread-$thread_number.sh
 
 	# Uncomment if your are using the intel compiler
 	#module load compiler/intel/2020/2 #compiler/gnu/9/2.0
-    echo "COMPILER=\"`icc --version |head -1`\"" >> submit-pthread-$thread_number.sh
+        #echo "COMPILER=\"`icc --version |head -1`\"" >> submit-pthread-$thread_number.sh
 
 	echo "TEMP=\`lscpu|grep \"Model name:\"\`" >> submit-pthread-$thread_number.sh
 	echo "IFS=':' read -ra CPU_MODEL <<< \"\$TEMP\"" >> submit-pthread-$thread_number.sh
@@ -56,12 +52,12 @@ do
 
 	echo "echo Run ./main-pthreads with $thread_number threads." >> submit-pthread-$thread_number.sh
 
-	echo "/usr/bin/time --format='%e' ./bin/main-pthreads --size $width $height --jpeg pthreads-$thread_number-${width}x$height.jpg --threads $thread_number 2> temp-pthread-$thread_number" >> submit-pthread-$thread_number.sh
+	echo "/usr/bin/time --format='%e' ./bin-gnu/main-pthreads --size $width $height --jpeg pthreads-$thread_number-${width}x$height.jpg --threads $thread_number 2> temp-pthread-$thread_number" >> submit-pthread-$thread_number.sh
 
 	echo "RUNTIME=\`cat temp-pthread-$thread_number\`" >> submit-pthread-$thread_number.sh
 
-    echo "echo \${CPU_MODEL[1]},Pthread,\$thread_number,1,\$COMPILER,\${width}x\$height,\$RUNTIME >> timing-pthread-$thread_number.csv" >> submit-pthread-$thread_number.sh
-    echo "#rm temp-pthread-$thread_number" >> submit-pthread-$thread_number.sh
+        echo "echo \${CPU_MODEL[1]},Pthread,\$thread_number,1,\$COMPILER,\${width}x\$height,\$RUNTIME >> timing-pthread-$thread_number.csv" >> submit-pthread-$thread_number.sh
+        echo "#rm temp-pthread-$thread_number" >> submit-pthread-$thread_number.sh
 
 	chmod +x submit-pthread-$thread_number.sh
 	sbatch submit-pthread-$thread_number.sh
